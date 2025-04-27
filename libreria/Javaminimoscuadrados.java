8546d6bc188983126a141ced2e992cc27d9c57da
angela aponte <angela.aponte01@uceva.edu.co>
2025-04-27 22:18:33 +0000

    creacion de metodo java

---
+++ libreria/Javaminimoscuadrados.java
@@ -0,0 +1,15 @@
+ package libreria;
+
+ public class Javaminimoscuadrados{
+
+ public native float suma (float[] vector, int n);
+
+ public native float sumaProducto(float[] vector1, float[] vector2, int n);
+
+ public native float sumaCuadrados(float[] vector, int n);
+
+ public native float calcularPendiente(float[] x, float[] y, int n);
+
+ public native float calcularOrdenada(float[] x, float[] y, int n, float pendiente);
+
+
